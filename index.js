54dd10ebe1ead7a4800d5f94a31bdf6999a748bd
Jerry <Jvictor940@gmail.com>
2022-04-01 17:59:22 -0400

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let div = document.getElementById('container')
-let = document.getElementById('')
+let arrivalTimesScreen = document.getElementById('arrival-times')
 let = document.getElementById('')
 let = document.getElementById('')
 
@@ -10,9 +10,33 @@ const getAllStations = async () => {
         // console.log('stations' , res)
         let h2 = document.createElement('h2')
         h2.innerText = element.name
-        div.append(h2)
+        h2.dataset.stationId = element.id 
+        h2.addEventListener('click', () => {
+            getArrivalTimes(h2.dataset.stationId)
+        })
+       
+        div.append(h2) 
+    })
 
-        
-    });
+    const displayArrivalTimes = (arrivalTimes) => {
+        arrivalTimes.forEach((element) => {
+            let h4 = document.createElement('h4')
+            h4.innerText = element
+            arrivalTimesScreen.append(h4)
+        })
+    }
+
+    const getArrivalTimes = async (stationId) => {
+        let req = await fetch(`https://mtaapi.herokuapp.com/api?id=${stationId}`)
+        let res = await req.json()
+        console.log('Arrival times', res.result.arrivals)
+        displayArrivalTimes(res.result.arrivals)
+    
+    }
 }
+/*const getArrivalTimes = async (stationId) => {
+    let req = await fetch(`https://mtaapi.herokuapp.com/api?id=${stationId}`)
+    let res = await req.json()
+    console.log('Arrival times', res.result.arrivals)
+}*/
 getAllStations()
