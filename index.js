bc086be029ddb0c91be15fea809c073db5e831a6
Jerry <Jvictor940@gmail.com>
2022-04-01 16:50:31 -0400

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+let div = document.getElementById('container')
+let = document.getElementById('')
+let = document.getElementById('')
+let = document.getElementById('')
+
 const getAllStations = async () => {
     let req = await fetch('https://mtaapi.herokuapp.com/stations')
     let res = await req.json()
-    console.log('stations' , res)
+    res.result.forEach((element) => {
+        // console.log('stations' , res)
+        let h2 = document.createElement('h2')
+        h2.innerText = element.name
+        div.append(h2)
+
+        
+    });
 }
+getAllStations()
